1e4cc97b20f3e60d1e5035c666c3b87913d4289b
Adam Morabito <amorabito@2ndwatch.com>
2021-06-17 16:21:21 -0700

    Formatting and better security

--- New-Password.ps1
+++ New-Password.ps1
@@ -1,14 +1,14 @@
 function New-Password {
-
     <#
     .SYNOPSIS
     Generates a random password.
 
     .DESCRIPTION
-    Generates a random plain text password consisting of either alphanumeric characters and non-alphanumeric characters
-    or only alphanumeric characters. To generate an encrypted password, pass the function to the ConvertTo-SecureString
-    cmdlet (see example). The default password length is 20 characters. The minimum character length is 16 and the
-    maximum is 2048.
+    Generates a random plaintext or SecureString password consisting of either alphanumeric characters and
+    non-alphanumeric (special) characters or only alphanumeric characters. The length can be changed and special
+    characters can be omitted. The password is always output directly to a SecureString object. It's a good idea to use
+    the SecureString.Dispose method to securely dispose of SecureString objects stored in memory once they have been
+    processed and are no longer needed (see example 4).
 
     .PARAMETER Length
     Specifies the password length. The default password length is 20 characters. The minimum character length is 16 and
@@ -17,11 +17,15 @@ function New-Password {
     .PARAMETER NoSpecialChars
     Excludes non-alphanumeric (special) characters.
 
+    .PARAMETER AsSecureString
+    Outputs the password as a SecureString object.
+
     .INPUTS
     None
 
     .OUTPUTS
     System.String
+    System.SecureString
 
     .EXAMPLE
     New-Password
@@ -33,12 +37,16 @@ function New-Password {
     New-Password -NoSpecialChars
 
     .EXAMPLE
-    $p = ConvertTo-SecureString -String (New-Password) -AsPlainText -Force
+    $password = New-Password -AsSecureString
+    $password.Dispose()
 
+    .NOTES
+    Requires PowerShell 7.0 or greater.
+    
     #>
 
     [CmdletBinding()]
-    param(
+    Param(
         [Parameter(
             Position = 0,
             Mandatory = $false
@@ -50,13 +58,20 @@ function New-Password {
             Position = 1,
             Mandatory = $false
         )]
-        [switch]$NoSpecialChars
+        [switch]$NoSpecialChars,
+
+        [Parameter(
+            Position = 2,
+            Mandatory = $false
+        )]
+        [switch]$AsSecureString
     )
 
-    begin {
-    # ASCII chars in decimal notation
+    # The Requires statement doesn't work in all execution cases so we do it this way
+    if ($PSVersionTable.PSVersion.Major -ge 7) {
 
-    $chars = @{
+        # Define ASCII character set in decimal format
+        $chars = @{
             Alphanumeric = @(
                 48..57
                 65..90
@@ -70,17 +85,22 @@ function New-Password {
                 91..95
             )
         }
-    }
 
-    process {
+        # Generate secure password
         if (-not $NoSpecialChars) {
-            $password = -join (($chars.Alphanumeric + $chars.NonAlphanumeric)*26 | Get-Random -Count $Length | ForEach-Object {[System.Char]$_})
+            $password = ConvertTo-SecureString -String (-join (($chars.Alphanumeric + $chars.NonAlphanumeric)*26 | Get-Random -Count $Length | ForEach-Object {[System.Char]$_})) -AsPlainText -Force
         } else {
-            $password = -join (($chars.Alphanumeric)*26 | Get-Random -Count $Length | ForEach-Object {[System.Char]$_})
+            $password = ConvertTo-SecureString -String (-join (($chars.Alphanumeric)*26 | Get-Random -Count $Length | ForEach-Object {[System.Char]$_})) -AsPlainText -Force
         }
-    }
 
-    end {
-        return $password
+        # Output password as plaintext string or SecureString object
+        if (-not $AsSecureString) {
+            ConvertFrom-SecureString -SecureString $password -AsPlainText
+            $password.Dispose()
+        } else {
+            return $password
+        }
+    } else {
+        Write-Warning -Message "The function New-Password requires at least version 7.0 of PowerShell to run. You are running version $($PSVersionTable.PSVersion.ToString())."
     }
 }
